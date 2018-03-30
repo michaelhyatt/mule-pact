2ecbb59c8903938d9a49aa220ed66b566ad0b536
michaelhyatt <michael.hyatt@mulesoft.com>
2018-03-30 21:07:49 +1100

    Initial

---
+++ client/src/test/java/client/RunClientTestToGeneratePact.java
@@ -0,0 +1,63 @@
+package client;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.mule.munit.runner.functional.FunctionalMunitSuite;
+
+import au.com.dius.pact.consumer.ConsumerPactBuilder;
+import au.com.dius.pact.consumer.ConsumerPactRunnerKt;
+import au.com.dius.pact.consumer.PactVerificationResult;
+import au.com.dius.pact.model.MockProviderConfig;
+import au.com.dius.pact.model.RequestResponsePact;
+
+public class RunClientTestToGeneratePact extends FunctionalMunitSuite {
+
+	private static final String TEST_SUITE_MUNIT_FILE = "client-test-suite.xml";
+	private static final String TEST_FLOW_NAME = "client-test-suite-clientCallTest";
+	private static final int PORT = 1234;
+	private static final String LOCALHOST = "localhost";
+
+	@Override
+	protected String getConfigResources() {
+		
+		System.setProperty("http.host", LOCALHOST);
+		System.setProperty("http.port", String.valueOf(PORT));
+				
+		return TEST_SUITE_MUNIT_FILE;
+	}
+
+	@Test
+	public void test() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("content-type", "application/json");
+		
+		RequestResponsePact pact = ConsumerPactBuilder
+				.consumer("Some Consumer")
+				.hasPactWith("Some Provider")
+				.uponReceiving("a request to say Hello")
+					.path("/")
+					.method("GET")
+				.willRespondWith().status(200)
+					.body("{\"result\":\"success\"}")
+					.headers(headers)
+				.toPact();
+
+		MockProviderConfig config = MockProviderConfig.createDefault();
+		config.setHostname(LOCALHOST);
+		config.setPort(PORT);
+		
+		PactVerificationResult result = ConsumerPactRunnerKt.runConsumerTest(pact, config, 
+				m -> runFlow(TEST_FLOW_NAME, testEvent("")));
+
+		if (result instanceof PactVerificationResult.Error) {
+			throw new RuntimeException(((PactVerificationResult.Error) result).getError());
+		}
+
+		assertEquals(PactVerificationResult.Ok.INSTANCE, result);
+	}
+
+}
---
+++ client/src/test/java/client/RunClientTestToGeneratePactWithDsl.java
@@ -0,0 +1,62 @@
+package client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.munit.runner.functional.FunctionalMunitSuite;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+
+public class RunClientTestToGeneratePactWithDsl extends FunctionalMunitSuite {
+	
+	private static final String HTTP_PORT_PROPERTY_NAME = "http.port";
+	private static final String HTTP_HOST_PROPERTY_NAME = "http.host";
+	private static final String CONSUMER = "SomeConsumer";
+	private static final String PROVIDER = "SomeProvider";
+	private static final String TEST_SUITE_MUNIT_FILE = "client-test-suite.xml";
+	private static final String TEST_FLOW_NAME = "client-test-suite-clientCallTest";
+	private static final int PORT = 1234;
+	private static final String LOCALHOST = "localhost";
+	
+	@Rule
+    public PactProviderRuleMk2 provider = new PactProviderRuleMk2(PROVIDER, LOCALHOST, PORT, this);
+
+	@Override
+	protected String getConfigResources() {
+		
+		System.setProperty(HTTP_HOST_PROPERTY_NAME, LOCALHOST);
+		System.setProperty(HTTP_PORT_PROPERTY_NAME, String.valueOf(PORT));
+				
+		return TEST_SUITE_MUNIT_FILE;
+	}
+	
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    public RequestResponsePact configurationFragment(PactDslWithProvider builder) {
+    	
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("content-type", "application/json");   	
+ 
+        return builder
+                .given("john smith books a civic")
+                .uponReceiving("retrieve data from Service")
+					.path("/")
+					.method("GET")
+				.willRespondWith().status(200)
+					.body("{\"result\":\"success\"}")
+					.headers(headers)
+				.toPact();
+    }
+    
+    @PactVerification(PROVIDER)
+    @Test
+    public void testFlow() throws Exception {
+    	
+		runFlow(TEST_FLOW_NAME, testEvent(""));
+    }
+}
